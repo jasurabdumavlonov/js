525cb8f09cd1a79220736c39d1cc13a86cede8ab
JR | Software Engineer <jasurackerman2004@gmail.com>
2023-10-26 08:18:41 +0500

    js

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+let myName = "Jasur"
+let programmingLanguage = "JavaScript"
+let courseCreator = "Владилен Минин"
+let reasonText = "Я хочу создать что то что изменит весь мир"
+let numberOfMonth = "2"
+console.log("Всем привет! Меня зовут "+`${myName}` + " Сейчас я изучаю язык программирования "+`${programmingLanguage}`+" на курсе по " + `${programmingLanguage}`+ " у " + `${courseCreator}`+" Я хочу стать веб разработчиком потому что "+ `${reasonText}` + " До этого я изучал " + `${programmingLanguage}` + `${numberOfMonth}` + " месяцев " + "Я уверен что пройду данный курс до конца!")
