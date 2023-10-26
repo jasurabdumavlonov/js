74bc35ec4d2c8f2da902ee63518f5a09e9042626
JR | Software Engineer <jasurackerman2004@gmail.com>
2023-10-26 11:29:55 +0500

    logic.js exercises

---
+++ JS/logic.js
@@ -0,0 +1,30 @@
+const isFrontendDeveloper = true
+if(isFrontendDeveloper){
+  console.log("Вы Front End разработчик добро пожаловать в команду!")
+}
+else{
+  console.log("Вы чето попутали")
+}
+
+const opentime = 8
+const closetime = 10
+if(opentime > closetime){
+  console.log("Магазин закрыт")
+}
+else{
+  console.log("Магазин закрыт")
+}
+
+const developer = "Front End"
+if(developer === "Front End"){
+  console.log("2000$")
+}
+else if(developer === 'Backend'){
+console.log("1500$")
+}
+else if(developer === "Full Stack"){
+  console.log("3000$")
+}
+else{
+  console.log("Зарплата не определена")
+}
--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,4 @@
-let myName = "Jasur"
-let programmingLanguage = "JavaScript"
-let courseCreator = "Владилен Минин"
-let reasonText = "Я хочу создать что то что изменит весь мир"
-let numberOfMonth = "2"
-console.log("Всем привет! Меня зовут "+`${myName}` + " Сейчас я изучаю язык программирования "+`${programmingLanguage}`+" на курсе по " + `${programmingLanguage}`+ " у " + `${courseCreator}`+" Я хочу стать веб разработчиком потому что "+ `${reasonText}` + " До этого я изучал " + `${programmingLanguage}` + `${numberOfMonth}` + " месяцев " + "Я уверен что пройду данный курс до конца!")
+console.log('500' > '51')
+console.log(undefined == null)
+console.log(undefined == NaN)
+console.log("123" === 1)
